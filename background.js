74b39c0ee70d11493284661b89deb58da0f45e9e
Rodolf Brillantes <rodolf.brillantes@skyscanner.net>
2016-05-16 15:47:08 +0800

    remove dns suffix

--- background.js
+++ background.js
@@ -12,7 +12,7 @@ chrome.runtime.onConnect.addListener(function(conn) {
                     }
                 }
             };
-            xhr.open('GET', 'http://seagull.corp.skyscanner.local/service_delivery/get_squad_from_websiteID_json?websiteID=' + msg.websiteID, true);
+            xhr.open('GET', 'http://seagull/service_delivery/get_squad_from_websiteID_json?websiteID=' + msg.websiteID, true);
             xhr.send();
         }
     });
